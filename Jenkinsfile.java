40928c49df852895da8fbd8e4563ef6f261204f7
omkar.kulkarni <omkar.kulkarni@fisglobal.com>
2021-02-19 09:19:16 -0600

    Commiting JenkinsFile

---
+++ Jenkinsfile.java
@@ -0,0 +1,44 @@
+pipeline {
+    agent any
+    tools { 
+            maven 'maven_3.6.3' 
+    }
+ 
+    stages {
+        stage('Checkout') {
+            steps {        
+            checkout([$class: 'GitSCM', branches: [[name: 'main']], extensions: [[$class: 'CheckoutOption', timeout: 5], [$class: 'CloneOption', noTags: false, reference: '', shallow: false, timeout: 5]], userRemoteConfigs: [[url: 'https://github.com/j-huidrom/devops.git']]])
+
+ 
+
+             echo 'Checkout source code from git'
+            }
+        }
+        stage('Quality Check') {
+            steps {
+                echo 'QA verified'
+            }
+        }
+        stage('Security Check') {
+            steps {
+                echo 'All security checks done'
+            }
+        }
+        stage('Build Push App') {
+            steps {
+               sh "mvn clean install"        
+        }
+        }    
+        
+         stage('Deploy') {
+            steps {
+                echo 'Deployment done'
+            }
+        }
+         stage('Post Deployment Check') {
+            steps {
+                echo 'All deployment check done'
+            }
+        }
+    }
+}
